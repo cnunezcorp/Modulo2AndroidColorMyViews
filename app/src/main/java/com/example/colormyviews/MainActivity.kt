36bfcd4f5a193fc581e059d9f04ad56e64c31ce1
CristoferNC <cristofercorporan675@hotmail.com>
2024-10-10 15:48:53 -0400

    Añadir archivos del proyecto

---
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.colormyviews
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.colormyviews.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // Inicializando el binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // Estableciendo la vista de contenido
+        setContentView(binding.root)
+        setListeners()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    private fun setListeners(){
+        val clickableViews: List<View> =
+            listOf(binding.boxOneText, binding.boxTwoText, binding.boxThreeText,
+                    binding.boxFourText, binding.boxFiveText, binding.main,
+                    binding.redButton, binding.yellowButton, binding.greenButton)
+
+        for (item in clickableViews){
+            item.setOnClickListener { makeColored(it) }
+        }
+    }
+
+    private fun makeColored(view: View) {
+        when (view.id){
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            R.id.red_button -> binding.boxThreeText.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> binding.boxFourText.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> binding.boxFiveText.setBackgroundResource(R.color.my_green)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+}
